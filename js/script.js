eb443e920092f592bcb14261474b944b52c54a1b
babд-gam <nikitalav21lin@gmail.com>
2021-06-17 18:34:10 +0300

    range

--- js/script.js
+++ js/script.js
@@ -1,38 +1,51 @@
-ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', { // 'map' это твой id в разметке, куда ты хочешь засунуть карту
-          center: [34.8697400, -111.7609900],
-          zoom: 10
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
+// popup
 
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
+const OpenPopupBtn = document.querySelector(".map-section-btn");
+const ClosePopupBtn = document.querySelector(".map-section-btn");
+const Popup = document.querySelector("popup");
 
-      myPlacemarkWithContent = new ymaps.Placemark([34.8697400,  -111.7609900], {
-          hintContent: 'Собственный значок метки с контентом',
-          balloonContent: 'HTML Academy, first level',
-      }, {
-          // Опции.
-          // Необходимо указать данный тип макета.
-          iconLayout: 'default#imageWithContent',
-          // Своё изображение иконки метки.
-          iconImageHref: 'https://pngicon.ru/file/uploads/geometka-256x256.png', // здесь мы указываем путь до нашей картинки пина (метки)
-          // Размеры метки.
-          iconImageSize: [40, 40], // здесь уже все просто и понятно , если переводить с английского названия свойств
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
-          iconImageOffset: [-27, -48],
-          // Смещение слоя с содержимым относительно слоя с картинкой.
-          iconContentOffset: [15, 15],
-          // Макет содержимого.
-          iconContentLayout: MyIconContentLayout
-      });
 
-  myMap.geoObjects
-      .add(myPlacemarkWithContent);
-});
+// range
 
+window.onload = function(){
+	slideOne();
+	slideTwo();
+}
+
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+
+let displayValuesOne = document.getElementById("range1");
+let displayValuesTwo = document.getElementById("range2");
+
+let minGap = 200;
+
+let sliderTrack = document.querySelector(".slider-tarck");
+let sliderMaxVal = document.getElementById("slider-1").max;
+
+
+function slideOne(){
+	if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){sliderOne.value = parseInt(sliderTwo.value) - minGap;
+	}
+	displayValuesOne.textContent = sliderOne.value;
+	fillColor();
+};
+
+function slideTwo(){
+	if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){sliderTwo.value = parseInt(sliderOne.value) + minGap;
+	}
+	displayValuesTwo.textContent = sliderTwo.value;
+	fillColor();
+};
+
+function fillColor() {
+	percent1 = (sliderOne.value / sliderMaxVal) *
+	100;
+	percent2= (sliderTwo.value / sliderMaxVal) *
+	100;
+	sliderTrack.style.background = `linear-gradient(to right, #ABABAB ${percent1}%
+	, #ffffff ${percent1}% , #ffffff ${percent2}%,
+	#ABABAB ${percent2}%)`;
+};
 

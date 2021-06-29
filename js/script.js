5e248e21cc4e6a7ceec50e35d18c393695f18466
babд-gam <nikitalav21lin@gmail.com>
2021-06-29 14:35:32 +0300

    пппппппппппппппппппппппппппппппппппппппппппппп

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const openClosePopupBtn = document.querySelector(".map-section-btn");
 const popup = document.querySelector(".popup");
 
 openClosePopupBtn.addEventListener("click", function() {
-  popup.classList.toggle("hidden");
+  popup.classList.toggle("modal-visibility");
 });
 
